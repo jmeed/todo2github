deab7dec1e412974de0ecec7748a2bccfbf31113
Jonathan Meed <jmeed@umich.edu>
2014-04-06 21:50:58 -0400

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,9 +1,3 @@
-#TODO [GH1]: automaticly load project info from directory
-#Project's account and name
-projectAccount = "jmeed"
-project = "todo2github"
-
-
 # your personal github user name and password
 #TODO [GH2]: automaticly load user and password from git settings
 user = '<user>'
